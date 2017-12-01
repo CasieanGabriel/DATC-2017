518d0e7311bf3fbf7283403721a7af69728a19ef
Daniel Iercan <diercan@isoftwaresystems.com>
2017-12-01 12:38:24 +0200

    updates

---
+++ BUTA GEANINA/CURS/TEMA1/Hal.Client_tema1/Hal.Client/Beers_class.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Hal.Client
+{
+
+    public class Beers
+    {
+        public Beers(int id, string name, int breweryId, string breweryName, BeersLinks _links)
+        {
+            Id = id;
+            Name = name;
+            BreweryId = breweryId;
+            BreweryName = breweryName;
+            this._links = _links;
+        }
+        public Beers(string name)
+        {
+            Name = name;           
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BreweryId { get; set; }
+        public string BreweryName { get; set; }
+        public BeersLinks _links { get; set; }
+
+        public static explicit operator Beers(JObject v)
+        {
+            return new Beers((int)v["Id"], (string)v["Name"], (int)v["BreweryId"], (string)v["BreweryName"], (BeersLinks)v["_links"]);
+        }
+    }
+    
+    public class SelfBeers
+    {
+        public SelfBeers(string href)
+        {
+            this.href = href;
+        }
+
+        public string href { get; set; }
+
+        public static explicit operator SelfBeers(JToken t)
+        {
+            return new SelfBeers((string)t["href"]);
+        }
+    }
+
+
+    public class BeersBrewerys
+    {
+        public BeersBrewerys(string href)
+        {
+            this.href = href;
+        }
+
+        public string href { get; set; }
+
+        public static explicit operator BeersBrewerys(JToken t)
+        {
+            return new BeersBrewerys((string)t["href"]);
+        }
+    }
+
+    public class BeersLinks
+    {
+        public BeersLinks(SelfBeers self, BeersBrewerys brewery)
+        {
+            this.self = self;
+            this.brewery = brewery;
+        }
+
+        public SelfBeers self { get; set; }
+        public BeersBrewerys brewery { get; set; }
+
+        public static explicit operator BeersLinks(JToken t)
+        {
+            return new BeersLinks((SelfBeers)t["self"],  (BeersBrewerys)t["brewery"]);
+        }
+    }
+
+    public class EmbeddedBeers
+    {
+        public EmbeddedBeers(List<Beers> beer)
+        {
+            this.beer = beer;
+        }
+
+        public List<Beers> beer { get; set; }
+
+        public static explicit operator EmbeddedBeers(JToken t)
+        {
+            List<Beers> BeersList = t["beer"].ToObject<List<Beers>>();
+            return new EmbeddedBeers(BeersList);
+        }
+    }
+
+    public class RootObjectBeers
+    {
+        public RootObjectBeers(int totalResults, int totalPages, int page, EmbeddedBeers _embedded)
+        {
+            TotalResults = totalResults;
+            TotalPages = totalPages;
+            Page = page;
+            this._embedded = _embedded;
+        }
+
+        public int TotalResults { get; set; }
+        public int TotalPages { get; set; }
+        public int Page { get; set; }
+        public EmbeddedBeers _embedded { get; set; }
+
+        public static explicit operator RootObjectBeers(JObject t)
+        {
+            return new RootObjectBeers((int)t["TotalResults"], (int)t["TotalPages"], (int)t["Page"], (EmbeddedBeers)t["_embedded"]);
+        }
+    }
+
+}
+
---
+++ BUTA GEANINA/CURS/TEMA1/Hal.Client_tema1/Hal.Client/Breweries_class.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Hal.Client
+{
+    class Breweries_class
+    {
+    }
+
+
+    public class Breweries
+    {
+        public Breweries(int id, string name, BreweriesLinks _links)
+        {
+            Id = id;
+            Name = name;
+            _links = _links;
+        }
+
+        public string href { get; set; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public BreweriesLinks _links { get; set; }
+
+        public static explicit operator Breweries(JObject v)
+        {
+            return new Breweries((int)v["Id"], (string)v["Name"], (BreweriesLinks)v["_links"]);
+        }
+    }
+
+    public class SelfBreweries
+    {
+        public SelfBreweries(string href)
+        {
+            this.href = href;
+        }
+
+        public string href { get; set; }
+
+        public static explicit operator SelfBreweries(JToken t)
+        {
+            return new SelfBreweries((string)t["href"]);
+        }
+    }
+
+
+
+    public class BreweriesBeers
+    {
+        public BreweriesBeers(string href)
+        {
+            this.href = href;
+        }
+
+        public string href { get; set; }
+
+        public static explicit operator BreweriesBeers(JToken t)
+        {
+            return new BreweriesBeers((string)t["href"]);
+        }
+    }
+
+    public class BreweriesLinks
+    {
+        public BreweriesLinks(SelfBreweries self, BreweriesBeers bere)
+        {
+            this.self = self;
+            this.bere = bere;
+        }
+
+        public SelfBreweries self { get; set; }
+        public BreweriesBeers bere { get; set; }
+
+        public static explicit operator BreweriesLinks(JToken t)
+        {
+            return new BreweriesLinks((SelfBreweries)t["self"], (BreweriesBeers)t["beer"]);
+        }
+    }
+
+    public class EmbeddedBreweries
+    {
+        public EmbeddedBreweries(List<Breweries> breweries)
+        {
+            this.breweries = breweries;
+        }
+
+        public List<Breweries> breweries { get; set; }
+
+        public static explicit operator EmbeddedBreweries(JToken t)
+        {
+            List<Breweries> BreweriesList = t["brewery"].ToObject<List<Breweries>>();
+            return new EmbeddedBreweries(BreweriesList);
+        }
+    }
+
+    public class RootObjectBrewery
+    {
+        public RootObjectBrewery(BreweriesLinks _links, EmbeddedBreweries _embedded)
+        {
+            _links = _links;
+            this._embedded = _embedded;
+        }
+
+        public BreweriesLinks _links { get; set; }
+        public EmbeddedBreweries _embedded { get; set; }
+
+        public static explicit operator RootObjectBrewery(JObject t)
+        {
+            return new RootObjectBrewery((BreweriesLinks)t["_links"], (EmbeddedBreweries)t["_embedded"]);
+        }
+    }
+
+}
+
