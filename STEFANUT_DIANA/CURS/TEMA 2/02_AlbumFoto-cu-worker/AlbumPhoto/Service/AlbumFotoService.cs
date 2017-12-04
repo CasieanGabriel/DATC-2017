a87a0b40b3a7488f8e2062800ed5f46cebb88e32
DianaStefanut <DianaStefanut@users.noreply.github.com>
2017-12-05 00:35:52 +0200

    tema2
    tema2

---
+++ STEFANUT_DIANA/CURS/TEMA 2/02_AlbumFoto-cu-worker/AlbumPhoto/Service/AlbumFotoService.cs
@@ -0,0 +1,152 @@
+﻿using AlbumPhoto.Models;
+using AlbumPhoto.Service.Entities;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Table.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Blob.Protocol;
+
+namespace AlbumPhoto.Service
+{
+	public class AlbumFotoService
+	{
+		private CloudStorageAccount _account;
+        private CloudBlobClient _blobClient;
+        private CloudBlobContainer _photoContainer;
+		private CloudTableClient _tableClient;
+		private CloudTable _filesTable;
+		private CloudTable _commentsTable;
+		private TableServiceContext _ctx;
+        private BlobContainerPermissions _blobContainterPermissions;
+        public static string SAS;
+
+        public AlbumFotoService()
+		{
+			//_account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("PhotoStorage"));
+			_account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+			_blobClient = _account.CreateCloudBlobClient();
+			_photoContainer = _blobClient.GetContainerReference("poze");
+			if (_photoContainer.CreateIfNotExists())
+			{
+				_photoContainer.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
+			}
+
+			_tableClient = _account.CreateCloudTableClient();
+			_filesTable = _tableClient.GetTableReference("files");
+			_filesTable.CreateIfNotExists();
+			_commentsTable = _tableClient.GetTableReference("comments");
+			_commentsTable.CreateIfNotExists();
+			_ctx = _tableClient.GetTableServiceContext();
+
+
+            _blobContainterPermissions = new BlobContainerPermissions();
+            _blobContainterPermissions.SharedAccessPolicies.Add("twohourspolicy", new SharedAccessBlobPolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(2),
+                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-1),
+                Permissions = SharedAccessBlobPermissions.Read
+            });
+            _blobContainterPermissions.PublicAccess = BlobContainerPublicAccessType.Off;
+            _photoContainer.SetPermissions(_blobContainterPermissions);
+            SAS = _photoContainer.GetSharedAccessSignature(new SharedAccessBlobPolicy(), "twohourspolicy");
+
+
+        }
+        public static string GetUrl(string fileName)
+         {
+            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient sasBlobClient = new CloudBlobClient(storageAccount.BlobEndpoint, new StorageCredentials(SAS));
+
+            CloudBlob blob = (CloudBlob)sasBlobClient.GetBlobReferenceFromServer(new Uri(fileName));
+            return blob.Uri.AbsoluteUri + SAS;
+        }
+
+    public List<Poza> GetPoze()
+		{
+			var poze = new List<Poza>();
+			var query = (from file in _ctx.CreateQuery<FileEntity>(_filesTable.Name)
+						 select file).AsTableServiceQuery<FileEntity>(_ctx);
+
+			foreach (var file in query)
+			{
+				poze.Add(new Poza()
+				{
+					Description = file.RowKey,
+					ThumbnailUrl = file.ThumbnailUrl,
+					Url = file.Url
+				});
+			}
+
+			return poze;
+		}
+
+		public void IncarcaPoza(string userName, string description, Stream continut)
+		{
+			var blob = _photoContainer.GetBlockBlobReference(description);
+			blob.UploadFromStream(continut);
+
+			_ctx.AddObject(_filesTable.Name, new FileEntity(userName, description)
+			{
+				PublishDate = DateTime.UtcNow,
+				Size = continut.Length,
+				Url = blob.Uri.ToString(),
+			});
+
+			_ctx.SaveChangesWithRetries();
+		}
+
+        public List<Comentariu> GetComm(string poza)
+        {
+            var listaComm = new List<Comentariu>();
+            if(!string.IsNullOrEmpty(poza))
+            {
+                var query = (from file in _ctx.CreateQuery<CommentEntity>(_commentsTable.Name) select file).AsTableServiceQuery<CommentEntity>(_ctx);
+                foreach(var comm in query)
+                {
+                    if (!string.IsNullOrEmpty(comm.Text))
+                        listaComm.Add(new Comentariu()
+                        {
+                            Text = comm.Text.Substring(poza.Length+1),
+                            MadeBy = comm.MadeBy,
+                        });
+                }
+            }
+            return listaComm;
+        }
+
+        public void IntroduComm(string id, string file, string text)
+        {
+            _ctx.AddObject(_commentsTable.Name, new CommentEntity(id, file)
+            {
+                Timestamp = DateTime.UtcNow,
+                MadeBy = "user",
+                Text = text
+               });
+            _ctx.SaveChangesWithRetries();
+
+        }
+
+        public string GenLink ( string poza )
+        {
+            var query = (from file in _ctx.CreateQuery<FileEntity>(_filesTable.Name)
+                             where file.RowKey == poza
+                             select file).AsTableServiceQuery<FileEntity>(_ctx);
+
+            foreach (var link in query)
+            {
+                return GetUrl(link.Url);
+            }
+
+            return "No link!";
+        }
+
+	}
+}
