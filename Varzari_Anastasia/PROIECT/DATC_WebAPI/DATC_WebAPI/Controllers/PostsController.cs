3a1469444e7b388343a142b4d99e94c414321ff5
VarzariAnastasia <varzarianastasia@yahoo.ro>
2018-01-08 23:37:32 +0200

    Project "Likes Counter"

---
+++ Varzari_Anastasia/PROIECT/DATC_WebAPI/DATC_WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DATC_WebAPI.Models;
+
+namespace DATC_WebAPI.Controllers
+{
+    public class PostsController : ApiController
+    {
+        CRUD_Table myTable = new CRUD_Table();
+
+        // GET api/values
+        public List<MyTable> Get()
+        {
+            return myTable.GetAllUsers();
+        }
+
+        // GET: api/Posts/5
+        public int Get(string id)
+        {
+            return Int32.Parse(myTable.GetUser(id).PostsLikes);
+        }
+
+        // POST: api/Posts
+        public void Post(string id, string postsLikes)
+        {
+            myTable.updatePostsLikes(id, postsLikes);
+        }
+
+        // PUT: api/Posts/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Posts/5
+        public void Delete(string id)
+        {
+            myTable.deleteData(id);
+        }
+    }
+}
---
+++ Varzari_Anastasia/PROIECT/DATC_WebAPI/DATC_WebAPI/Controllers/UserNameController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DATC_WebAPI.Models;
+
+namespace DATC_WebAPI.Controllers
+{
+    public class UserNameController : ApiController
+    {
+        CRUD_Table myTable = new CRUD_Table();
+
+        // GET api/values
+        public List<MyTable> Get()
+        {
+            return myTable.GetAllUsers();
+        }
+
+        // GET: api/Posts/5
+        public string Get(string id)
+        {
+            return myTable.GetUser(id).RowKey;
+        }
+
+        // POST: api/UserName
+        public void Post(string id, string name)
+        {
+            myTable.PostUserNameAndID(id, name);
+        }
+
+        // PUT: api/UserName/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/UserName/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
