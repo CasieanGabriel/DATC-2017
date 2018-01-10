9426835d58b122161e66f100489bc61a0f698834
paultarce <paultarce@gmail.com>
2018-01-10 23:45:29 +0200

    Moblie App
    Thread

---
+++ Tarce Paul/PROIECT/Aplicatie Mobila/Datc_Map2/Datc_Map2/Models/Senzor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Datc_Map2.Models
+{
+    class Senzor
+    {
+        public int ParkingId { get; set; }
+        public int Status { get; set; }
+    }
+}
