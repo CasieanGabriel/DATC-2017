5879d643a7cf78908a500a4468a94b1a50b78b56
Voloacă Maria-Alexandra <32413796+mariavoloaca@users.noreply.github.com>
2018-01-11 13:45:22 +0200

    app

---
+++ Voloaca Maria/Proiect/Timeia/Output/Database/ParkingLot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Output.Models
+{
+    public class ParkingLot
+    {
+        [Key]
+        public int LotId { get; set; }
+        public decimal TopLeftLat { get; set; }
+        public decimal TopLeftLng { get; set; }
+        public decimal TopRightLat { get; set; }
+        public decimal TopRightLng { get; set; }
+        public decimal BottomRightLat { get; set; }
+        public decimal BottomRightLng { get; set; }
+        public decimal BottomLeftLat { get; set; }
+        public decimal BottomLeftLng { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
---
+++ Voloaca Maria/Proiect/Timeia/Output/Output/Models/ParkingContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace Output.Models
+{
+    public class ParkingContext : DbContext
+    {
+        public DbSet<ParkingLot> ParkingLots { get; set; }
+    }
+}
