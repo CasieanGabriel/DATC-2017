46c45e6754fd3efaf80e0fd621d70779c6d3c060
LarisaTranca <larisa.tranca15@gmail.com>
2018-01-13 19:45:06 +0200

    Tema 2

---
+++ TRANCA_LARISA_CRISTINA/CURS/TEMA 2/AlbumFoto-cu-worker/02_AlbumFoto-cu-worker/AlbumPhoto/Controllers/CommentsController.cs
@@ -0,0 +1,47 @@
+﻿using AlbumPhoto.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlbumPhoto.Controllers
+{
+    public class CommentsController : Controller
+    {
+        //
+        // GET: /Comments/
+
+        public ActionResult Index()
+        {
+            var service = new AlbumFotoService();
+            return View(service.GetPoze());
+        }
+
+        [HttpPost]
+        public ActionResult AdaugaComentariu(string comentariu, string username, string poza)
+        {
+            var service = new AlbumFotoService();
+            if (comentariu != null && comentariu.Length > 0)
+            {
+                service.AdaugaComentariu(comentariu,username, poza);
+            }
+
+            return View("Index", service.GetPoze());
+        }
+
+        [HttpGet]
+        public ActionResult getComments(string poza)
+        {
+            var service = new AlbumFotoService();
+            return View("Comments", service.getComments(poza));
+        }
+
+        [HttpPost]
+        public ActionResult GenerareLink(string poza)
+        {
+            var service = new AlbumFotoService();
+            return View("GenerareLink", service.GenerareLink(poza));
+        }
+    }
+}
